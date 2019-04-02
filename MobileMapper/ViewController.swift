68dff22b7e787a141c8c4b7f5aa5b3573405e150
Peggy Hunt <phunt@d125.org>
2019-04-02 10:11:44 -0500

    Search button finding parks

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var parks: [MKMapItem] = []
+    
     let locationManager = CLLocationManager()
     
     var currentLocation: CLLocation!
@@ -43,6 +45,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func searchButtonTapped(_ sender: UIBarButtonItem) {
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        
+        search.start { (response, error) in
+            guard let response = response else { return }
+            for mapItem in response.mapItems {
+                self.parks.append(mapItem)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+        
+        
         
     }
     
